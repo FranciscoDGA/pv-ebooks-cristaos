8978fe37130a2b24a36b99c5a739f9cb9f2c4e7b
Francisco Gomes <afiliadopebas@gmail.com>
2026-06-10 14:10:14 -0300

    fix: resolve syntax error on App.tsx and finalize Contact route

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import { BlogIndex } from './pages/BlogIndex';
 import { ArticlePage } from './pages/ArticlePage';
 import { LandingPage } from './components/LandingPage';
 import { PlaceholderPage } from './pages/PlaceholderPage';
-import { LegalPage } from './pages/LegalPage';\nimport { ContactPage } from './pages/ContactPage';
+import { LegalPage } from './pages/LegalPage';
+import { ContactPage } from './pages/ContactPage';
 
 import { configOrando } from './config/configOrando';
 import { configRei } from './config/configRei';
@@ -27,8 +28,8 @@ function App() {
           {/* Páginas Extras */}
           <Route path="em-breve" element={<PlaceholderPage />} />
           <Route path="contato" element={<ContactPage />} />
-          <Route path="privacidade" element={<LegalPage title="Política de Privacidade"><p>A Editora Coleção do Reino tem o compromisso com a privacidade e a segurança de seus clientes durante todo o processo de navegação e compra pelo site.</p><p>Os dados cadastrais dos clientes não são vendidos, trocados ou divulgados para terceiros, exceto quando essas informações são necessárias para o processo de entrega, para cobrança, ou para participação em promoções solicitadas pelos clientes.</p></LegalPage>} />
-          <Route path="termos" element={<LegalPage title="Termos de Uso"><p>Ao acessar o site Coleção do Reino, você concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis.</p><p>Os materiais exibidos no site podem incluir erros técnicos, tipográficos ou fotográficos. A Coleção do Reino não garante que qualquer material em seu site seja preciso, completo ou atual.</p></LegalPage>} />
+          <Route path="privacidade" element={<LegalPage title="Política de Privacidade"><p>A Editora O Chamado da Graça tem o compromisso com a privacidade e a segurança de seus clientes durante todo o processo de navegação e compra pelo site.</p><p>Os dados cadastrais dos clientes não são vendidos, trocados ou divulgados para terceiros, exceto quando essas informações são necessárias para o processo de entrega, para cobrança, ou para participação em promoções solicitadas pelos clientes.</p></LegalPage>} />
+          <Route path="termos" element={<LegalPage title="Termos de Uso"><p>Ao acessar o site O Chamado da Graça, você concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis.</p><p>Os materiais exibidos no site podem incluir erros técnicos, tipográficos ou fotográficos. A Editora O Chamado da Graça não garante que qualquer material em seu site seja preciso, completo ou atual.</p></LegalPage>} />
 
           {/* Livros Landing Pages */}
           <Route path="orando" element={<LandingPage config={configOrando} />} />
